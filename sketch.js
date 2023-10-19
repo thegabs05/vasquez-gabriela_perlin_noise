b36f65aee8064b33fb96e343be8b4a63e32ce0aa
Gabriela Vasquez Gutierrez <gabriela.vsquezg@gmail.com>
2023-10-19 17:16:04 -0500

    Perlin noise primer ejercicio

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
+var inc = 0.01;
+var start = 0;
+
+let xoof = 0.0;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  background(170, 25, 236);
+  stroke(255);
+  noFill();
+  beginShape();
+  var xoff = start;
+
+  for (var x = 0; x < windowWidth; x++) {
+    stroke(255, random(150, 200, 120), 255, 85);
+    strokeWeight(random(10, 15));
+    var y = noise(xoff) * windowHeight;
+    vertex(x, y);
+
+    xoff += inc;
+  }
+
+  xoof = xoof + 0.01;
+  let n = noise(xoof) * windowWidth;
+  //fill(0);
+  circle(n, 100, 12);
+
+  endShape();
+  start += inc;
+  //noloop();
 }
